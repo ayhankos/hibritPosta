75f514b7d494dc9054c55ee034ffa08fe7b4521c
ayhankos <ayhanx926@gmail.com>
2024-06-22 15:59:26 +0300

    postgre to mysql

--- utils/user/getUserRoles.ts
+++ utils/user/getUserRoles.ts
@@ -1,14 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Role } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export async function getUserRoles() {
-  const roles = await prisma.$queryRaw<{ enum_value: string }[]>`
-    SELECT unnest(enum_range(NULL::"Role"))::text AS enum_value;
-  `;
+  const roles = Object.values(Role);
 
   const specificRoles = ["COMPANY_ADMIN", "COMPANY_STAFF"];
-  return roles
-    .map((role) => role.enum_value)
-    .filter((role) => specificRoles.includes(role));
+  return roles.filter((role) => specificRoles.includes(role));
 }
